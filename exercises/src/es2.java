3327b7814e9f8f8f8a52037932dc9f05145a35d1
Leon35-fc <leon.f35@alice.it>
2026-01-05 16:53:54 +0100

    Update es2.java

--- exercises/src/es2.java
+++ exercises/src/es2.java
@@ -1,3 +1,8 @@
 void main() {
-    
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Ciao inserisci una stringa");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Ciao inserisci una stringa");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Ciao inserisci una stringa");
 }
